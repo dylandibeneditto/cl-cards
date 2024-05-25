35f30f555664883800d4fb5777aa42b12afba5f8
Dylan DiBeneditto <dibenedittod@gmail.com>
2024-05-25 19:52:58 -0400

    feat: updated algorithm logic

--- algorithms.py
+++ algorithms.py
@@ -1,10 +1,54 @@
-def flashcards(params):
-    print("hello from flashcards")
+"""
+params: dict {
+    'index': int,                           # Current index of the flashcard
+    'data': list                            # The flashcards data
+    'currentCard': {front: str, back: str}  # The current flashcard
+    'user_input': str                       # Input from the user
+}
+"""
 
-def written(params):
-    print("hello from written")
+class FlashcardsAlgorithm:
+    def __init__(self):
+        self.flipped = 0
+        
+    def initialDisplay(self):
+        print("- `n` | move to next card")
+        print("- `b` | move back one card")
+        print("- `f` | flip card")
+        print("[ enter to start ]")
+        
+    def display(self, params):
+        print(params['currentCard'][self.flipped])
 
-a = {
-    "flashcards": flashcards,
-    "written": written
+    def logic(self, params):
+        # Example algorithm: Show next flashcard in sequence, unless user inputs 'repeat'
+        next_index = params['index']
+        if params['user_input'] == 'n':
+            next_index = (params['index'] + 1) % len(params['data'])
+        elif params['user_input'] == 'b':
+            next_index = (params['index'] - 1) % len(params['data'])
+        elif params['user_input'] == 'f':
+            self.flipped = 1-self.flipped
+        return next_index
+
+
+class WrittenAlgorithm:
+    def __init__(self):
+        self.state = {}
+
+    def next_index(self, params):
+        # Example algorithm: Go to the next flashcard unless user inputs 'repeat'
+        if params['user_input'] == 'repeat':
+            next_index = params['index']
+        else:
+            next_index = (params['index'] + 1) % len(params['data'])
+        return next_index
+
+# Dictionary of algorithms
+algorithms = {
+    # !!! DONT REMOVE "flashcards" !!!
+    # flashcards is the automatic choice when entering the program
+    # removing it will permanantly break the program until it is restored
+    "flashcards": FlashcardsAlgorithm,
+    "written": WrittenAlgorithm
 }
--- main.py
+++ main.py
@@ -1,52 +1,90 @@
 import os
 import glob
-from algorithms import a
+from algorithms import algorithms
+from load import load_set
 
 currentSet = "main.set"
 currentAlg = "flashcards"
 currentCmd = None
+currentlyRunning = False
+currentIndex = 0
+flashcardsData = load_set(currentSet)
+currentAlgorithmInstance = algorithms[currentAlg]()
 
 def header():
-    global currentCmd, currentSet, currentAlg
-    print(f'{f" ${currentCmd}" if currentCmd else f"{currentSet} > {currentAlg}"}'.center(os.get_terminal_size().columns))
-    a[currentAlg]("hello")
+    global currentCmd, currentSet, currentAlg, currentIndex, currentlyRunning
+    header_str = f'{f" ${currentCmd}" if currentCmd else f""}'
+    if currentlyRunning:
+        header_str = f"{currentSet} | {currentIndex+1} / {len(flashcardsData)}"
+    elif currentCmd:
+        header_str = f" ${currentCmd}"
+    else:
+        header_str = f"{currentSet} > {currentAlg}"
+    print(header_str.center(os.get_terminal_size().columns))
+
+def run():
+    global currentIndex, currentlyRunning
+    user_input = ""
+    clear()
+    currentAlgorithmInstance.initialDisplay()
+    while(user_input != "exit"):
+        user_input = input()
+        clear()
+        params = {
+            'index': currentIndex,
+            'data': flashcardsData,
+            'currentCard': flashcardsData[currentIndex],
+            'user_input': user_input
+        }
+        currentIndex = currentAlgorithmInstance.logic(params)
+        currentAlgorithmInstance.display(params)
+    currentlyRunning = False
 
 def clear():
     os.system('cls' if os.name == "nt" else "clear")
     header()
 
 def cset(newSet):
-    global currentSet
-    if newSet.endswith(".set") == False:
+    global currentSet, flashcardsData, currentIndex
+    if not newSet.endswith(".set"):
         newSet += ".set"
     currentSet = newSet
+    flashcardsData = load_set(newSet)
+    currentIndex = 0
 
 def calg(newAlg):
-    global currentAlg
-    currentAlg = newAlg
+    global currentAlg, currentAlgorithmInstance
+    if newAlg in algorithms:
+        currentAlgorithmInstance = algorithms[newAlg]()
+        currentAlg = newAlg
+    else:
+        print(f"Algorithm '{newAlg}' not found.")
 
 def main():
-    global currentCmd
+    global currentCmd, currentlyRunning
     while True:
         cmd = input("> ")
         currentCmd = cmd
         clear()
 
-        if cmd=="help":
-            print("cset: change set\ncalg: change algorithm")
+        if cmd == "help":
+            print("cset: change set\ncalg: change algorithm\nrun: start running the flashcards")
 
         elif cmd == "cset":
             files = glob.glob("./*.set")
-            print("\n".join(files) if len(files)>0 else "You don't have any sets")
-            if len(files)>0:
-                newSet = input("?: ")
+            print("\n".join(files) if files else "You don't have any sets")
+            if files:
+                newSet = input("Enter new set: ")
                 cset(newSet)
         elif cmd == "calg":
-            newAlg = input("?: ")
+            newAlg = input("Enter new algorithm: ")
             calg(newAlg)
+        elif cmd == "run":
+            currentlyRunning = True
+            run()
         else:
-            print("unknown command:", cmd)
-        
+            print(f"Unknown command: {cmd}")
+
         currentCmd = None
         input("[ enter ]")
         clear()
